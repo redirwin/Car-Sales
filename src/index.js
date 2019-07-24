c3b898a8f6e9f40fe7adfe0d2086223c2f9b24be
Dave Irwin <david@redirwin.com>
2019-07-24 15:11:38 -0700

    Moved state from App, created and exported reducer.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import rootReducer from "./reducers/reducer";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
 import App from "./App";
 
-const store = createStore(reducer);
-
-function reducer() {
-  return {
-    message: "Hello from reducer!"
-  };
-}
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
